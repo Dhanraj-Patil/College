80702d02f435871bb6d0e5cb82fb2cf346820575
Dhanraj-Patil <69854547+Dhanraj-Patil@users.noreply.github.com>
2022-02-26 15:59:08 +0530

    26/2/22

--- Networks/src/CNFAT.java
+++ Networks/src/CNFAT.java
@@ -4,19 +4,19 @@ import java.util.*;
 import java.io.IOException;
 public class CNFAT {
     public static void main(String[] args) throws IOException{
-        System.out.println("Name: Dhanraj Patil\nRegistration No.: 20MIS0447");
+//        System.out.println("Name: Dhanraj Patil\nRegistration No.: 20MIS0447");
         Scanner sc=new Scanner(System.in);
         InetAddress address=InetAddress.getLocalHost();
         DatagramSocket ds=new DatagramSocket();
-        System.out.print("Enter a Year: ");
+        System.out.print("Enter a number: ");
         String n=sc.nextLine();
-        byte [] year=n.getBytes(StandardCharsets.UTF_8);
-        DatagramPacket dsend=new DatagramPacket(year,year.length,address,8080);
+        byte [] number=n.getBytes(StandardCharsets.UTF_8);
+        DatagramPacket dsend=new DatagramPacket(number,number.length,address,8080);
         ds.send(dsend);
         byte [] responce=new byte[1000];
         DatagramPacket drecieve=new DatagramPacket(responce,responce.length);
         ds.receive(drecieve);
-        System.out.println("Leap Year: "+convert(responce));
+        System.out.println("Armstrong number: "+convert(responce));
     }
     public static StringBuilder convert(byte[] a){
         if (a == null)
--- Networks/src/CNFATServer.java
+++ Networks/src/CNFATServer.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 public class CNFATServer {
     public static void main(String [] args) throws IOException{
-        System.out.println("Name: Dhanraj Patil\nRegistration No.: 20MIS0447");
+//        System.out.println("Name: Dhanraj Patil\nRegistration No.: 20MIS0447");
         System.out.println("Server running on Port 8080");
         DatagramSocket ds=new DatagramSocket(8080);
-        byte [] year=new byte[1000];
-        DatagramPacket drecieve=new DatagramPacket(year,year.length);
+        byte [] number=new byte[1000];
+        DatagramPacket drecieve=new DatagramPacket(number,number.length);
         ds.receive(drecieve);
-        System.out.println(convert(year));
-        String ans=answer(convert(year));
+        System.out.println(convert(number));
+        String ans=answer(convert(number));
         byte [] responce=ans.getBytes(StandardCharsets.UTF_8);
         DatagramPacket dsend=new DatagramPacket(responce,responce.length,drecieve.getAddress(),drecieve.getPort());
         ds.send(dsend);
@@ -29,8 +29,17 @@ public class CNFATServer {
         return build;
     }
     public static String answer(StringBuilder y){
-        int year=Integer.parseInt(String.valueOf(y));
-        if(year%4==0){
+        int n=Integer.parseInt(String.valueOf(y));
+        int m=n;
+        int d=0;
+        int sum=0;
+        while(n>0){
+            d=n%10;
+            sum=(int)(sum+Math.pow(d,3));
+            n=n/10;
+        }
+        System.out.println(sum);
+        if(sum==m){
             return "Yes";
         }else{
             return "No";
